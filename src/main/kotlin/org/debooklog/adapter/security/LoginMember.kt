9e6c75dc308121412c008dff834fc1a9dbb4590d
Duhan Mo <duhandv@gmail.com>
2025-01-26 23:46:14 +0900

    fix: hide object added LoginMember annotation from swagger

--- src/main/kotlin/org/debooklog/adapter/security/LoginMember.kt
+++ src/main/kotlin/org/debooklog/adapter/security/LoginMember.kt
@@ -1,5 +1,8 @@
 package org.debooklog.adapter.security
 
+import io.swagger.v3.oas.annotations.Parameter
+
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
+@Parameter(hidden = true) // Swagger 문서에서 숨김
 annotation class LoginMember
